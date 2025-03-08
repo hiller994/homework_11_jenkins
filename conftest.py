b798f343cd42d1aeaf05020b22761d7238b8c9a1
Andrey Ignatov <hiller994@gmail.com>
2025-03-09 00:35:13 +0300

    added video attach

--- conftest.py
+++ conftest.py
@@ -32,6 +32,7 @@ def setup_browser():
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
+    attach.add_video(browser)
 
 
     browser.quit()
--- utils/attach.py
+++ utils/attach.py
@@ -13,13 +13,13 @@ def add_logs(browser):
 
 
 def add_html(browser):
-    html = browser.driver.page_source #верни мне source страницы
+    html = browser.driver.page_source #верни мне source страницы, выдаст кусок сайта финального шага
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
 
 def add_video(browser):
-    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4" #ссылка на сам файл. формируется при каждом запуске теста
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
-           + "' type='video/mp4'></video></body></html>"
-    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
+           + "' type='video/mp4'></video></body></html>" #плеер HTML
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html') # далее выдаем ссылку и html код
